bd8ed90db87bf4ad854323b25f010ea134646337
vinaykumar3117 <35131302+vinaykumar3117@users.noreply.github.com>
2018-10-04 09:34:58 +0530

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -38,13 +38,24 @@ with tf.Session() as sess:
     # Run the initializer
     sess.run(init)
 
+    #print("mnist.train.num_examples: ", (int(mnist.train.num_examples)))
+
     # Training cycle
     for epoch in range(training_epochs):
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+
         sess.run(optimizer, feed_dict={x: batch_xs, y: batch_ys})
         # Display logs per epoch step
         if (epoch+1) % 1000 == 0:
             print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(sess.run(cost, feed_dict={x: batch_xs, y: batch_ys})))
+            #print ("The predicted value is:", np.array(sess.run(pred, feed_dict={x: batch_xs, y: batch_ys})).tolist()) # here since the batch size is 100 it gets 100 predicted vectors.
+            #print("The label is:", np.array(sess.run(y,feed_dict={y: batch_ys})).tolist()) # the actual image in the form 9 column vector like [0,0,1,0 ..]
+            predicted_value_vector = np.array(sess.run(pred, feed_dict={x: batch_xs, y: batch_ys})).tolist()
+            actual_label_vector = np.array(sess.run(y,feed_dict={y: batch_ys})).tolist()
+            #print("actual value vector", actual_label_vector)
+            #print("predicted_value_vector value vector", predicted_value_vector);
+            for actual_vector, predicted_vector in zip(actual_label_vector, predicted_value_vector):
+                print("Actual :-> %d :|: Predicted :-> %d" % (actual_vector.index(max(actual_vector)),predicted_vector.index(max(predicted_vector))))
 
     print("Optimization Finished!")
 
@@ -54,3 +65,22 @@ with tf.Session() as sess:
     # Calculate accuracy
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     print("Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
+
+
+"""
+Output:
+Epoch: 1000 cost= 0.222599283
+Actual :-> 8 :|: Predicted :-> 8
+Actual :-> 5 :|: Predicted :-> 5
+Actual :-> 8 :|: Predicted :-> 8
+Actual :-> 0 :|: Predicted :-> 0
+Actual :-> 5 :|: Predicted :-> 6
+Actual :-> 2 :|: Predicted :-> 2
+Actual :-> 8 :|: Predicted :-> 8
+Actual :-> 6 :|: Predicted :-> 6
+Actual :-> 1 :|: Predicted :-> 1
+...
+Optimization Finished!
+Accuracy: 0.9197
+"""
+
